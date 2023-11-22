75158a290e64c650b648154d8950dc8e2e5f6f74
jh0ny2k2 <jchaman2306@g.educaand.es>
2023-11-22 20:28:28 +0100

    recompensa

--- recompensa.py
+++ recompensa.py
@@ -9,11 +9,11 @@ def reward_function(params):
     steps = params['steps']
 
     # Parámetros de penalización y recompensa
-    collision_penalty = 1e-4
-    off_track_penalty = 1e-5
-    speed_reward = 0.8
-    steering_penalty = 0.6
-    center_reward = 0.9
+    collision_penalty = 0.6
+    off_track_penalty = 0.6
+    speed_reward = 0.9
+    steering_penalty = 0.8
+    center_reward = 0.5
     progress_reward = 1
 
     # Penalizar si el coche se sale del circuito
@@ -25,14 +25,14 @@ def reward_function(params):
         return collision_penalty
     
     # Penalizar cambios bruscos en el ángulo de dirección
-    if steering_angle > 20:
+    if steering_angle > 25:
         return steering_penalty
     
     # Recompensar por mantenerse cerca de la línea central
     reward = center_reward * (1.0 - (distance_from_center / (0.5 * track_width)))
     
     # Recompensar la velocidad en secciones rectas
-    if speed < 1.5 and speed >= 3:
+    if speed > 1.25:
         reward += speed_reward
     
     # Recompensar por el progreso en la pista
